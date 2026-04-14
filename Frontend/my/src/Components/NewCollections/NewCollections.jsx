db1a0a40e8c276e6707635ede25d20990d046b04
raees03730 <ransari60212@gmail.com>
2026-04-14 22:11:48 +0530

    Update NewCollections.jsx

--- Frontend/my/src/Components/NewCollections/NewCollections.jsx
+++ Frontend/my/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const [new_collection,setNew_collection] = useState([]);
     
   useEffect(()=>{
-    fetch('http://localhost:4000/newcollection')
+    fetch('https://e-commerce-backend-qe3u.onrender.com/newcollection')
     .then((response)=>response.json())
     .then((data)=>setNew_collection(data));
   },[])
